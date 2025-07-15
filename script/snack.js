de138b2557e1bc0a851f93674f4b260f053a76f3
GiordanoCasale <giordanocasale8@gmail.com>
2025-07-15 12:53:48 +0200

    [upd] snack 1 completed

---
+++ script/snack.js
@@ -0,0 +1,20 @@
+//snack 1
+
+function somma(a, b) {
+    return a + b;
+}
+
+
+console.log(somma(2, 3));
+
+const sommaAnonima = function (a, b) {
+    return a + b;
+}
+
+console.log(sommaAnonima(3, 4))
+
+const sommaFreccia = (a, b) => {
+    return a + b;
+}
+
+console.log(sommaFreccia(5, 5))
